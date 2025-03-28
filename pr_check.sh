a4be996f11a72f4b458dbe2db3f8af43245171e0
Pavel Sturc <psturc@redhat.com>
2025-03-28 06:27:24 +0100

    test: sealights with cypress plugin

--- pr_check.sh
+++ pr_check.sh
@@ -5,14 +5,49 @@
 # -------------------------------------------
 echo "Running UI from commit sha ${HEAD_SHA}"
 cd konflux-ui
+
+set -x
+
+export COMPONENT NODE_DEBUG BUILD_NAME SL_fileExtensions BUILD_DIR_PATH BABYLON_PLUGINS BSID
+
+# BUILD_NAME="konflux-ui-$(date -u +"%s")"
+# TODO: update
+COMPONENT=nodejs-test
+SL_fileExtensions=".js,.jsx,.ts,.tsx"
+BABYLON_PLUGINS="jsx,typescript"
+BUILD_DIR_PATH="dist"
+SL_BUILD_DIR_PATH="sl_dist"
+
+NODEJS_AGENT_IMAGE=quay.io/konflux-ci/tekton-integration-catalog/sealights-nodejs:latest
+
+podman run --network host --userns=keep-id --group-add keep-groups -v "$PWD:/konflux-ui" --workdir /konflux-ui -e NODE_DEBUG=sl \
+    $NODEJS_AGENT_IMAGE \
+    /bin/bash -cx "whoami && slnodejs prConfig --appName ${COMPONENT} --targetBranch ${TARGET_BRANCH} --repositoryUrl ${FORKED_REPO_URL} --latestCommit ${HEAD_SHA} --pullRequestNumber ${PR_NUMBER} --token ${SEALIGHTS_TOKEN}"
+    # repositoryUrl ?
+
+BSID=$(< buildSessionId)
+
 ./connect_to_local_konflux.sh
+
 yarn install
 
 # start the UI from the PR check in background, save logs to file
 yarn start > yarn_start_logfile 2>&1 &
+
 YARN_PID=$!
 
-set -x
+while ! ls ${BUILD_DIR_PATH} &> /dev/null; do
+  echo "waiting until the directory ${BUILD_DIR_PATH} is created"
+  sleep 5
+done
+
+podman run --network host --userns=keep-id --group-add keep-groups -v "$PWD:/konflux-ui" --workdir /konflux-ui -e NODE_DEBUG=sl \
+    $NODEJS_AGENT_IMAGE \
+    /bin/bash -cx "slnodejs scan --buildsessionidfile buildSessionId --scm git --workspacepath ${BUILD_DIR_PATH} --token ${SEALIGHTS_TOKEN} --outputpath ${SL_BUILD_DIR_PATH} --babylonPlugins ${BABYLON_PLUGINS} --instrumentForBrowsers"
+
+
+cp -r ${SL_BUILD_DIR_PATH}/* ${BUILD_DIR_PATH}
+
 
 # -------------------------------------
 # ----------- RUN E2E TESTS -----------
@@ -46,10 +81,13 @@ COMMON_SETUP="-v $PWD/artifacts:/tmp/artifacts:Z,U \
     -e CYPRESS_KONFLUX_BASE_URL=https://localhost:8080 \
     -e CYPRESS_USERNAME=${CYPRESS_USERNAME} \
     -e CYPRESS_PASSWORD=${CYPRESS_PASSWORD} \
-    -e CYPRESS_GH_TOKEN=${CYPRESS_GH_TOKEN}"
+    -e CYPRESS_GH_TOKEN=${CYPRESS_GH_TOKEN} \
+    -e CYPRESS_SL_TOKEN=${SEALIGHTS_TOKEN} \
+    -e CYPRESS_SL_BUILD_SESSION_ID=${BSID} \
+    -e CYPRESS_SL_TEST_STAGE=konflux-ui-e2e"
 
 TEST_RUN=0
-set +e
+
 podman run --network host ${COMMON_SETUP} ${TEST_IMAGE} || TEST_RUN=1
 
 # kill the background process running the UI
